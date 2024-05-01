b4021feb213c437ef752284e93582f0db0a1c602
iecharak <iecharak@kali.kali>
2024-05-01 21:39:01 +0100

    evrerything in progress

--- cleanup_docker.sh
+++ cleanup_docker.sh
@@ -8,7 +8,10 @@ docker rm $(docker ps -aq)     # Remove all containers, including stopped ones
 docker container prune -f
 
 # Remove unused images (both dangling and tagged)
-docker image prune -af
+#docker image prune -af
+
+docker rmi mariadb  nginx  wordpress
+
 
 # Clean up Docker cache (if needed)
 # Be cautious with this step
@@ -20,14 +23,19 @@ docker builder prune -f
 # Remove unused volumes
 docker volume prune -f
 
+docker volume ls -q | xargs -r docker volume rm
+
 # Remove unused networks
 docker network prune -f
 
+docker network ls -q | xargs -r docker network rm
+
 # Remove dangling images
-docker image prune -af
+#docker image prune -af
 
 # Alternatively, use docker system prune to remove all unused data
-docker system prune -f
+#docker system prune -f
 
 # Remove all unused data (containers, images, volumes, networks)
-docker system prune -af
+#docker system prune -af
+
