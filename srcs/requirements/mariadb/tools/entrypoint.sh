d96fe84e6e99107a15e535cef789952fb224861e
iecharak <iecharak@kali.kali>
2024-05-07 13:55:27 +0100

    done

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-# Perform any pre-startup tasks here if needed
-
-# Start MariaDB service
-exec /usr/bin/mysqld_safe --skip-syslog --datadir="/var/lib/mysql"
--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -28,31 +28,3 @@ fi
 
 # Start PHP-FPM
 /usr/sbin/php-fpm7.3 -F
-
-# #!/bin/sh
-
-# #check if wp-config.php exist
-# # if [ -f ./wp-config.php ]
-# # then
-# # 	echo "wordpress already downloaded"
-# # else
-
-# #Download wordpress and all config file
-# sleep 10
-# wget http://wordpress.org/latest.tar.gz
-# tar xfz latest.tar.gz
-# mv wordpress/* .
-# rm -rf latest.tar.gz
-# rm -rf wordpress
-
-# #Inport env variables in the config file
-# sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-# sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-# sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-# sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-# cp wp-config-sample.php wp-config.php
-
-# # fi
-
-# # exec "$@"
-# /usr/sbin/php-fpm7.3 -F
