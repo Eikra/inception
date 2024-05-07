b59cbfea1d78b2d76011da23d3261c9fa3ab154a
iecharak <iecharak@kali.kali>
2024-05-07 12:38:49 +0100

    done

--- srcs/requirements/mariadb/tools/setup_mariadb.sh
+++ srcs/requirements/mariadb/tools/setup_mariadb.sh
@@ -5,29 +5,19 @@ service mysql start
 
 # Create a new database
 echo "Creating new database..."
-mysql -e "CREATE DATABASE $MYSQL_DATABASE;"
+mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
 
 # Create a new user and grant privileges to the database
 echo "Creating new user and granting privileges..."
-mysql  -e "CREATE USER '$MYSQL_USER'@'$MYSQL_HOSTNAME' IDENTIFIED BY '$MYSQL_PASSWORD';"
-# mysql  -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'$MYSQL_HOSTNAME';"
-mysql -e " GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-# mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'iecharak'@'%' IDENTIFIED BY 'new1234';"
-
-
-mysql  -e "CREATE USER '$WP_USR'@'$MYSQL_HOSTNAME' IDENTIFIED BY '$WP_PWD';"
-# mysql  -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$WP_USR'@'$MYSQL_HOSTNAME';"
-mysql -e " GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$WP_USR'@'%' IDENTIFIED BY '$WP_PWD';"
-
-
-mysql  -e "FLUSH PRIVILEGES;"
+mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'$MYSQL_HOSTNAME' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mysql -e "FLUSH PRIVILEGES;"
 
 echo "Database setup complete!"
 
-# # Keep the container running
-# exec "$@"
 # Shutdown MariaDB service
-mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
+# mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
+service mysql stop
 
 # Start MariaDB server
 mysqld_safe
--- srcs/requirements/wordpress/tools/create_wordpress.sh
+++ srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -9,16 +9,19 @@ else
     tar xfz latest.tar.gz
     mv wordpress/* .
     rm -rf latest.tar.gz wordpress
+    # chown -R www-data:www-data /var/www/html/wordpress
 
     # Import environment variables into the config file
+    sleep 15
     sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
     sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
     sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
     sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
     cp wp-config-sample.php wp-config.php
     wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-    # wp user create --allow-root  $WP_USR $WP_EMAIL --user_pass=$WP_PWD 
-    wp user create --allow-root  $WP_USR $WP_EMAIL --user_pass=$WP_PWD  --path=/var/www/html/wordpress/
+
+    # Create the second user
+    wp user create $WP_USR $WP_EMAIL --user_pass=$WP_PWD --allow-root
 
     echo "WordPress installed successfully"
 fi
